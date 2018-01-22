4edece20ecdd71eefb2adeda4e2f690794003855
Endy Muhardin <endy.muhardin@gmail.com>
2018-01-22 22:54:20 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/googleanalytics/belajargoogleanalytics/BelajarGoogleanalyticsApplication.java
@@ -0,0 +1,12 @@
+package com.muhardin.endy.belajar.googleanalytics.belajargoogleanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarGoogleanalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarGoogleanalyticsApplication.class, args);
+	}
+}
